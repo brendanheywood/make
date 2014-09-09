1856872cff9bbbb3e7fcfb1778b45875e6aae88c
Brendan Heywood <brendan@catalyst-au.net>
2014-09-09 12:12:02 +1000

    Added mysql init target

--- database/mysql/load.mk
+++ database/mysql/load.mk
@@ -1,4 +1,6 @@
 #
+LocalDBSudoUser ?= root
+
 refresh-database:
 	@echo "== refresh-database =="
 	@echo
@@ -8,7 +10,10 @@ sql-cli:
 	@mysql -u $(LocalDBUser) --password='$(LocalDBPass)' $(LocalDBName)
 
 init-database:
-	# create the db locally
-
+	# WARNING this will fail if you run it a second time TODO make idempotent
+	# Create the local Database
+	sudo -u $(LocalDBSudoUser) mysql -e 'CREATE DATABASE IF NOT EXISTS $(LocalDBName) DEFAULT CHARACTER SET UTF8 COLLATE utf8_unicode_ci;'
+	# Create the local DB user and grant access
+	sudo -u $(LocalDBSudoUser) mysql -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,CREATE TEMPORARY TABLES,DROP,INDEX,ALTER ON $(LocalDBName).* TO $(LocalDBUser)@localhost IDENTIFIED BY '$(LocalDBPass)'";
 
 
