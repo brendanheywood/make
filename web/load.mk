47011e4715449deeedea39bbd0e4cba0e7897cfb
Brendan Heywood <brendan@catalyst-au.net>
2014-10-27 12:11:43 +1100

    Added support for local dev log tailing

--- web/load.mk
+++ web/load.mk
@@ -1,5 +1,5 @@
 #
-# This is for all targets related to apps that serve http
+# This is for all targets related to apps that serve http via apache
 #
 # There may end up being a similar driver setup to enable different
 # implementations depending on apache / nginx / unicorn etc
@@ -10,6 +10,10 @@ ifneq ($(wildcard $(LocalWebRoot)),)
 $(error "LocalWebRoot isn't set! Should be something like '/var/www/blah' $(LocalWebRoot)")
 endif
 
+LocalWebDir      = $(shell pwd)
+LocalWebConf     = $(shell grep -l $(LocalWebDir) /etc/apache2/sites-enabled/*)
+LocalWebErrorLog = $(shell grep ErrorLog $(LocalWebConf) | sed 's/ErrorLog//' | sed 's/\s//g' | sed 's/^/\/var\/log\/apache2/'  )
+
 #
 web-open:
 	@echo "URL: $(LocalWebRoot)"
@@ -19,15 +23,15 @@ config-web:
 	##########################
 	# Web config             #
 	##########################
+	@echo " LocalWebDir:   $(LocalWebDir)"
 	@echo " LocalWebRoot:  $(LocalWebRoot)"
+	@echo " LocalWebConf:  $(LocalWebConf)"
+	@echo " LocalWebErrorLog:  $(LocalWebErrorLog)"
+	@echo " LocalWebAccessLog: $(LocalWebAccessLog)"
 	@echo
 
 tail-error:
-	# reach into apache
-	# detect logfile
-	# if needed detect rsyslog
-	# trace path
-	tail -f $(LocalWebLog)
+	tail -f $(LocalWebErrorLog) | sed 's/\\n/\n/g'
 
 init-web:
 	@# no-op, could create apache conf?
